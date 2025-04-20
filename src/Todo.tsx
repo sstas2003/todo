fea2d1a03768f4017feb7fe61cc28324a2907fb6
seroshtanov_s <seroshtanov_s@bi.group>
2025-04-20 16:52:27 +0500

    fix app

---
+++ src/Todo.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+interface TodoItem {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+const Todo: React.FC = () => {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const handleAddTodo = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
+      setNewTodo('');
+    }
+  };
+
+  const handleToggleComplete = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleAddTodo}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new task..."
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            {todo.text}
+            <button onClick={() => handleToggleComplete(todo.id)}>Complete</button>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
+
